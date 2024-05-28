0742f040e8cb06823ff2e6437dd930cfac52b4de
glaubluiz <98503395+glaubluiz@users.noreply.github.com>
2024-05-28 20:27:50 -0300

    adicionado o EP GET do pagamento

--- src/main/java/com/tdesi/senai/pagamento/controller/PagamentoController.java
+++ src/main/java/com/tdesi/senai/pagamento/controller/PagamentoController.java
@@ -2,6 +2,7 @@ package com.tdesi.senai.pagamento.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -27,5 +28,10 @@ public class PagamentoController {
     public ResponseEntity insert (@RequestBody Pagamento pagamento) {
         return ResponseEntity.ok(service.insert(pagamento));
     }
-	
+
+    @RequestMapping(method = RequestMethod.GET, value = "/{id}")
+    public ResponseEntity finById(@PathVariable Long id){
+        return ResponseEntity.ok().body(service.findById(id));
+	}
+
 }
